e46a57be2f23b9d3a135d12e80c8791380428089
Carlos Godoi <cgodoi@Carlos-MBP.bbrouter>
2022-11-09 16:27:50 -0300

    feat: add async thunk

--- src/features/players/components/ListPlayers/index.tsx
+++ src/features/players/components/ListPlayers/index.tsx
@@ -1,8 +1,23 @@
-import { useSelector } from 'react-redux'
-import { RootState } from '../../../../app/store';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+import { AppDispatch, RootState } from '../../../../app/store';
+import { fetchAllPlayers } from '../../playersSlice';
 
 export function ListPlayers() {
-  const players = useSelector((state: RootState) => state.players);
+  const { players, loading } = useSelector((rootState: RootState) => rootState.players);
+  const dispatch = useDispatch<AppDispatch>()
+
+  useEffect(() => {
+    dispatch(fetchAllPlayers())
+  }, [])
+
+  if (loading === 'pending') {
+    return <p>Buscando jogadores...</p>
+  }
+
+  if (loading === 'failed') {
+    return <p>Erro ao buscar jogadores.</p>
+  }
 
   if (players.length < 1) {
     return <p>Não há jogadores registrados.</p>
--- src/features/players/playersSlice.ts
+++ src/features/players/playersSlice.ts
@@ -1,16 +1,52 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { Player } from '../../models/Player';
 
-const initialState: Player[] = []
+export const fetchAllPlayers = createAsyncThunk('users/fetchAll', async (): Promise<Player[]> => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve([
+        {
+          name: 'Player 1',
+        },
+        {
+          name: 'Player 2',
+        }
+      ])
+      // reject()
+    }, 1000)
+  })
+})
+
+interface State {
+  players: Player[]
+  loading: 'idle' | 'pending' | 'succeeded' | 'failed'
+}
+
+const initialState: State = {
+  players: [],
+  loading: 'idle',
+}
 
 export const playerSlice = createSlice({
   name: 'players',
   initialState,
   reducers: {
     addPlayer(state, { payload }: PayloadAction<Player>) {
-      state.push(payload);
+      state.players.push(payload);
     },
-  }
+  },
+  extraReducers(builder) {
+    builder.addCase(fetchAllPlayers.pending, state => {
+      state.loading = 'pending'
+    })
+    builder.addCase(fetchAllPlayers.fulfilled, (state, action) => {
+      state.loading = 'succeeded'
+      state.players = action.payload
+    })
+    builder.addCase(fetchAllPlayers.rejected, (state, action) => {
+      state.loading = 'failed'
+    })
+  },
 })
 
 export const { addPlayer } = playerSlice.actions
